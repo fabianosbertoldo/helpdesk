36d78c73b53390d1bb1964cb3f5988900c536604
fabianosbertoldo <fabianobertoldo@gmail.com>
2023-02-12 13:40:43 -0300

    perfil de teste criado

---
+++ src/main/java/com/fabiano/helpdesk/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.fabiano.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import com.fabiano.helpdesk.services.DBService;
+
+@Configurable
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void instanciaDB() {
+		
+		this.dbService.instanciaDB();
+	}
+	
+}
