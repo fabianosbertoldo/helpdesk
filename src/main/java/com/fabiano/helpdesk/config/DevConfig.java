e6a45c5b2f52a13100f3578dbe65ec02d4ef03b1
fabianosbertoldo <fabianobertoldo@gmail.com>
2023-02-12 16:02:53 -0300

    perfil de desenvolvimento

--- src/main/java/com/fabiano/helpdesk/config/DevConfig.java
+++ src/main/java/com/fabiano/helpdesk/config/DevConfig.java
@@ -2,29 +2,22 @@ package com.fabiano.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 import com.fabiano.helpdesk.services.DBService;
 
 @Configurable
-@Profile("dev")
-public class TestConfig {
+@Profile("test")
+public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String value;
 	
 	@Bean
-	public boolean instanciaDB() {
+	public void instanciaDB() {
 		
-		if(value.equals("create")) {
-			this.dbService.instanciaDB();
-			
-		}
-		return false;
+		this.dbService.instanciaDB();
 	}
 	
 }
